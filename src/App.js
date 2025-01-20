133221ece7340a348f3e1e2f7a0020801ba515e9
Khalid <Khalid>
2025-01-20 23:19:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple E-Commerce Cart</h1>
+    </div>
+  );
+};
+
+export default App;
